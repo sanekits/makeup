60c6f65d08d4f34d088e0889a8c99b47ec9cc526
LES MATHESON <LMATHESON4@bloomberg.net>
2024-01-17 15:16:34 +0000

    Improve shell quoting

--- bin/make-debug.sh
+++ bin/make-debug.sh
@@ -10,6 +10,38 @@ die() {
     builtin exit 1
 }
 
+do_help() {
+    cat <<-EOF | cut -c 6-
+
+    |Additional options for make-debug.sh:
+    |  --light: Use -xc instead of -uxec for .SHELLFLAGS
+EOF
+}
+
+ENABLE_HELP_APPEND=false
 Logfile=/tmp/make.log
-make -rd ".SHELLFLAGS= -uxec" "$@" |& tee ${Logfile}
-echo "Output make captured in $Logfile" >&2
+FwdArgs=()
+Shellflags="-uxec"
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --help|-h)
+            ENABLE_HELP_APPEND=true
+            FwdArgs+=("$1")
+            ;;
+        --light)
+            Shellflags="-xc"
+            ;;
+        *)
+            FwdArgs+=("$1")
+            ;;
+    esac
+    shift
+done
+{
+    printf "%s " "[make-debug.sh $(date -Iseconds)] Invoking: make -rd \".SHELLFLAGS= ${Shellflags}\""
+    printf "%s " "${FwdArgs[@]}"
+    printf "\n"
+    make -rd ".SHELLFLAGS= ${Shellflags}" "${FwdArgs[@]}"
+    $ENABLE_HELP_APPEND && do_help
+    echo "[make-debug.sh $(date -Iseconds)] Output of make was captured in $Logfile"
+} |& tee /tmp/make.log
