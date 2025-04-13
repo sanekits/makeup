888c9451ff7519180e8481dd63c966d410ce5a2c
LES MATHESON <LMATHESON4@bloomberg.net>
2025-04-13 10:32:38 +0000

    Reconcile with template v2

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ TODO: describe this shellkit
 
 Download and install the self-extracting setup script:
 ```
-curl -L https://github.com/sanekits/makeup/releases/download/0.2.1/makeup-setup-0.2.1.sh \
+curl -L https://github.com/sanekits/makeup/releases/download/0.2.2/makeup-setup-0.2.2.sh \
     -o ~/tmp$$.sh && bash ~/tmp$$.sh && rm ~/tmp$$.sh
 ```
 
--- bin/makeup.sh
+++ bin/makeup.sh
@@ -3,6 +3,15 @@
 #  This script can be removed if you don't need it -- and if you do
 # that you should remove the entry from _symlinks_ and make-kit.mk also.
 
+
+# The shellkit/ tooling naturally evolves out from under the dependent kits.  ShellkitSetupVers allows
+# detecting the need for refresh of templates/* derived files.  To bump the root version, 
+# zap all templates/* containing 'ShellkitTemplateVers' constants and changes to the corresponding dependent kits
+# Note that within templates/* there may be diverse versions in upstream shellkit, they don't all have to match,
+# but the derived copies should be sync'ed with upstream as needed.
+#shellcheck disable=2034
+ShellkitTemplateVers=2
+
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
         realpath.sh -f "$@"
@@ -13,14 +22,13 @@ canonpath() {
         return
     }
     # Fallback: Ok for rough work only, does not handle some corner cases:
-    ( builtin cd -L -- "$(command dirname -- $0)"; builtin echo "$(command pwd -P)/$(command basename -- $0)" )
+    ( builtin cd -L -- "$(command dirname -- "$0")" || exit; builtin echo "$(command pwd -P)/$(command basename -- "$0")" )
 }
 
 scriptName="$(canonpath "$0")"
-scriptDir=$(command dirname -- "${scriptName}")
 
 die() {
-    builtin echo "ERROR($(command basename -- ${scriptName})): $*" >&2
+    builtin echo "ERROR($(command basename -- "${scriptName}")): $*" >&2
     builtin exit 1
 }
 
